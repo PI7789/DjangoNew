75612a40ccbfe6ba9276f34f9f7e0cd9c459fe66
Piotr Idzik <160234244@student.rnngroup.ac.uk>
2025-02-11 14:45:02 +0000

    123

--- mango/website/forms.py
+++ mango/website/forms.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from .models import HotelUser, Booking
 
 from django import forms
 from django.forms.widgets import PasswordInput, TextInput
 
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = HotelUser
+
+        fields = ['first_name', 'last_name', 'username', 'email', 'phonenum', 'password']
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
--- mango/website/views.py
+++ mango/website/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import HotelUser
+
+from .forms import RegisterForm, LoginForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'pages/index.html')
+
+def register(request):
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('')
+    context = {'form': form}
+
+    return render(request, 'pages/register.html' , context=context)
+
+def login(request):
+    form = LoginForm
+
+    if request.method == "POST":
+        form = LoginForm(request data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request,user)
+                return redirect('')
+            
+
